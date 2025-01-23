7bcb4bd6e4832705154e8407762f2613adfc931b
Dániel K <51174697+DanCodernaut@users.noreply.github.com>
2025-01-23 16:45:37 +0100

    modified footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -31,7 +31,7 @@ const Footer = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Enter your e-mail"
-            className="w-full px-4 py-2 border border-black/70 focus:outline-none focus:border-blue-500"
+            className="w-full px-4 py-2 border border-black/70 focus:outline-none focus:border-blue-500 rounded-md"
             required
           />
           {isSubscribed && (
@@ -42,7 +42,7 @@ const Footer = () => {
         </div>
         <button
           type="submit"
-          className="bg-black text-white px-4 py-2 hover:bg-gray-800"
+          className="bg-black text-white px-4 py-2 hover:bg-gray-800 rounded-md"
         >
           →
         </button>
