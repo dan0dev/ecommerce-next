c50710840fab20e4ecb09662f76c5641104d4c63
Dániel K <51174697+DanCodernaut@users.noreply.github.com>
2025-01-24 11:27:59 +0100

    basic structure changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,24 @@
 import BentoHomeSection from "./components/BentoHomeSection";
 import Categories from "./components/Categories";
 import Collections from "./components/collection/Collections";
-import CookieBanner from "./components/CookieBanner";
-import Footer from "./components/Footer";
 import LastPart from "./components/LastPart";
 import MiddleBanner from "./components/MiddleBanner";
-import Navbar from "./components/Navbar";
+import PageTransition from "./components/PageTransition";
 import Topbar from "./components/Topbar";
-import TopInfo from "./components/TopInfo";
 
 export default function App() {
   return (
     <main>
-      <CookieBanner />
-      <TopInfo />
-      <Navbar />
-      <div className="max-w-screen-xl mx-auto">
-        <Topbar />
-        <BentoHomeSection />
-        <Categories />
-        <Collections />
-        <MiddleBanner />
-        <LastPart />
-      </div>
-      <Footer />
+      <PageTransition>
+        <div className="max-w-screen-xl mx-auto">
+          <Topbar />
+          <BentoHomeSection />
+          <Categories />
+          <Collections />
+          <MiddleBanner />
+          <LastPart />
+        </div>
+      </PageTransition>
     </main>
   );
 }
